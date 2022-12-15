6ceda8f7418e77dc7760ca049caa4e3ab3814e8b
wangyu <wangyu@onchain.com>
2022-12-15 15:11:57 +0800

    m

--- batch_submitter/batch-submitter.sh
+++ batch_submitter/batch-submitter.sh
@@ -4,14 +4,16 @@ set -e
 
 RETRIES=${RETRIES:-40}
 
-if [[ ! -z "$URL" ]]; then
+echo 1
+#if [[ ! -z "$URL" ]]; then
     # get the addrs from the URL provided
     ADDRESSES=$(curl --fail --show-error --silent --retry-connrefused --retry $RETRIES --retry-delay 5 $URL)
+echo $ADDRESSES
     # set the env
     export CTC_ADDRESS=$(echo $ADDRESSES | jq -r '.CanonicalTransactionChain')
     export SCC_ADDRESS=$(echo $ADDRESSES | jq -r '.StateCommitmentChain')
-fi
-
+#fi
+echo 2
 
 # waits for l2geth to be up
 curl --fail \
@@ -22,6 +24,7 @@ curl --fail \
     --retry-delay 1 \
     --output /dev/null \
     $L2_ETH_RPC
+echo 3
 
 # go
 exec ./batch-submitter "$@"
